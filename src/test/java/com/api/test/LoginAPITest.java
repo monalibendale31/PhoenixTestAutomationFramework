3675d0af6633a6e622fb1be4de6e48576021209d
monalibendale31 <monalibendale31@gmail.com>
2026-03-30 14:46:02 +0530

    Base code for my automation framework

---
+++ src/test/java/com/api/test/LoginAPITest.java
@@ -0,0 +1,44 @@
+package com.api.test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
+
+import org.testng.annotations.Test;
+
+import com.api.pojo.UserCredentials;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class LoginAPITest {
+	
+	@Test
+	public void validateLoginAPITest() {
+	
+		UserCredentials usercred=new UserCredentials("iamfd","password");
+		
+		given()
+		.baseUri("http://64.227.160.186:9000/v1")
+		.and()
+		.contentType(ContentType.JSON)
+		.accept(ContentType.ANY)
+		.body(usercred)
+		.log().uri()
+		.log().headers()
+		.log().method()
+	
+		.when()
+		.post("login")
+		.then()
+		.log().all()
+		.statusCode(200)
+		.and()
+		.body("message",equalTo("Success"))
+		.and()
+		.time(lessThan(1000L))
+		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/loginResponseSchema.json"));
+		
+	}
+
+}
